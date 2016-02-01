4d7bf66ee9661b0d287e3df592a4058b1ab15f37
Katie Sipos <ohitsmekatie@github.com>
2016-01-31 21:47:43 -0800

    add module to check events

--- alien_invaders.py
+++ alien_invaders.py
@@ -1,22 +1,19 @@
-
-import sys
 import pygame
-# import game settings for screen width,height,color
+
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
-# the function that starts the game & defines the screen
 def run_game():
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
+
     ship = Ship(screen)
 
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events()
 
         screen.fill(ai_settings.bg_color)
         ship.blitme()
--- ship.py
+++ ship.py
@@ -1,6 +1,7 @@
 import pygame
 
 class Ship():
+    
     def __init__(self, screen):
         self.screen = screen
         self.image = pygame.image.load('images/ship.bmp')
